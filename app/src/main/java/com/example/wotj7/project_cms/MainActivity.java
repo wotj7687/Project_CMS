426a1e4966e819ec32fcf72828765c226d22625a
wotj7687 <wotj7687@naver.com>
2019-05-09 16:20:56 +0900

    Initial commit

---
+++ app/src/main/java/com/example/wotj7/project_cms/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wotj7.project_cms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
